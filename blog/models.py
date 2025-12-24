64d663eeed6a5ff9efac6a47529758889a22013c
shubhamkumar-dianapps <shubham.kumar@dianapps.com>
2025-12-24 11:49:42 +0530

    added SEO Friendly Canonical URL

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -40,4 +40,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,14 @@ def post_list(request):
     return render(request, "post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=slug,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+
     return render(request, "post/detail.html", {"post": post})
