e6e82058a11917eaf738c2baaab0d4032f2a6f13
shubhamkumar-dianapps <shubham.kumar@dianapps.com>
2025-12-24 12:22:39 +0530

    Utilize Paginator Class to achieve Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "post/list.html", {"posts": posts})
+    paginator = Paginator(posts, 3)  # 3 posts per page
+    page_number = request.GET.get("page", 1)
+    try:
+        post = paginator.get_page(page_number)
+    except EmptyPage:
+        post = paginator.get_page(paginator.num_pages)
+    except PageNotAnInteger:
+        post = paginator.get_page(1)
+    return render(request, "post/list.html", {"posts": post})
 
 
 def post_detail(request, year, month, day, slug):
